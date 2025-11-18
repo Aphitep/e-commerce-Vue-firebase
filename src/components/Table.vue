65e2e9548330e60087f3526bd178fc6527265c72
Aphitep <aumaphitep@gmail.com>
2025-11-18 09:37:09 +0700

    add table component

---
+++ src/components/Table.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+    header: Array,
+});
+</script>
+<template>
+    <table class="table">
+        <!-- head -->
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Image</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Status</th>
+                <th>Update At</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <slot></slot>
+        </tbody>
+    </table>
+</template>
