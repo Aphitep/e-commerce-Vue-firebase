8a16444060685f79a64790483df9b92a49ba5ecd
Aphitep <aumaphitep@gmail.com>
2025-11-18 12:32:07 +0700

    update user list

--- src/views/admin/user/UpdateView.vue
+++ src/views/admin/user/UpdateView.vue
@@ -1,4 +1,20 @@
 <script setup>
 import AdminLayout from "@/layouts/AdminLayout.vue";
+
+const updateUser = () => {};
 </script>
-<template><AdminLayout>User Update</AdminLayout></template>
+<template>
+    <AdminLayout>
+        <div class="shadow-2xl p-4 m-4">
+            <div class="text-2xl font-bold">User Update</div>
+            <div class="divider"></div>
+            <div></div>
+            <div class="flex justify-between w-full mt-3">
+                <button class="btn btn-ghost">back</button>
+                <button class="btn btn-neutral" @click="updateUser">
+                    Update
+                </button>
+            </div>
+        </div>
+    </AdminLayout>
+</template>
