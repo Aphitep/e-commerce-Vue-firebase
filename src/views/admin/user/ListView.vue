717c8d95ce3f0e998ae19d4befe9ae84b7e5d3f2
Aphitep <aumaphitep@gmail.com>
2025-11-18 11:15:21 +0700

    update list view

--- src/views/admin/user/ListView.vue
+++ src/views/admin/user/ListView.vue
@@ -1,15 +1,56 @@
 <script setup>
+import { ref } from "vue";
+import { RouterLink } from "vue-router";
+
 import AdminLayout from "@/layouts/AdminLayout.vue";
+
+import { useAdminUserStore } from "@/stores/admin/user";
+
 import Trash from "@/components/icons/Trash.vue";
 import Edit from "@/components/icons/Edit.vue";
 import Table from "@/components/Table.vue";
+
+const adminUserStore = useAdminUserStore();
+const changeStatus = (index) => {
+    let selectedUser = adminUserStore.list[index];
+
+    if (selectedUser.status === "active") {
+        selectedUser.status = "inactive";
+    } else {
+        selectedUser.status = "active";
+    }
+    adminUserStore.updateUser(index, selectedUser);
+};
 </script>
 <template>
     <AdminLayout>
         <div>
             <div class="text-3xl font-bold m-4">User List</div>
             <Table :header="['Name', 'Role', 'Status', 'Update at', '']">
-                <tr></tr>
+                <tr v-for="(user, index) in adminUserStore.list">
+                    <td>{{ user.fullname }}</td>
+                    <td>{{ user.role }}</td>
+                    <td>{{ user.status }}</td>
+                    <td>{{ user.updateAt }}</td>
+                    <td>
+                        <div class="flex gap-1">
+                            <RouterLink
+                                :to="{
+                                    name: 'admin-user-update',
+                                    params: { id: index },
+                                }"
+                                class="btn"
+                                >Edit</RouterLink
+                            ><button class="btn" @click="changeStatus(index)">
+                                {{
+                                    user.status === "active"
+                                        ? "Disable"
+                                        : "Enable"
+                                }}
+                            </button>
+                        </div>
+                    </td>
+                </tr>
             </Table>
         </div>
     </AdminLayout>
