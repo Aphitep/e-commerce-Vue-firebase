19c207fc42ade041920235496767c35e32ae9224
Aphitep <aumaphitep@gmail.com>
2025-11-20 12:42:14 +0700

    add firebase

--- src/stores/user/products.js
+++ src/stores/user/products.js
@@ -1,4 +1,6 @@
 import { defineStore } from "pinia";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/firebase";
 
 export const useProductStore = defineStore("product", {
   state: () => ({
@@ -8,10 +10,16 @@ export const useProductStore = defineStore("product", {
     filterProducts(query) {
       return this.list.filter((product) => product.name.includes(query));
     },
-    loadProduct() {
-      const products = localStorage.getItem("admin-product");
-      if (products) {
-        this.list = JSON.parse(products);
+    async loadProduct() {
+      try {
+        const productSnapshot = await getDocs(collection(db, "products"));
+        const products = productSnapshot.docs.map((doc) => doc.data());
+        console.log(products);
+        if (products) {
+          this.list = products;
+        }
+      } catch (error) {
+        console.log("Error", error);
       }
     },
   },
